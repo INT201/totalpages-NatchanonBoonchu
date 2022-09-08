e9f6a24a3bfcc446c92e90c1bfe26f5a4f5f0341
NatchanonBoonchu <natchanon.boonchu@mail.kmutt.ac.th>
2022-09-08 22:36:57 +0700

    update code

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,14 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  
+  if (arrayItems === null || arrayItems === undefined) {
+    return undefined;
+  }else if(rowsPerPage === null || rowsPerPage === undefined || rowsPerPage == 0) {
+    return 1;
+  }else {
+    let x = arrayItems.length/rowsPerPage
+    return Math.ceil(x);
+  }
 }
 module.exports = totalPages
